444f2307854df21b8f29ffd73e2faa88147e0a63
Jovan Simonovic <jovan.simonovic02@gmail.com>
2024-08-19 14:26:35 +0200

    Feature: Added Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import LogIn from "./pages/LogIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
-  return <div className="text-2xl">Test</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/login" exact element={<LogIn />} />
+        <Route path="/signup" exact element={<SignUp />} />
+        <Route path="/profile" exact element={<Profile />} />
+        <Route path="/about" exact element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
