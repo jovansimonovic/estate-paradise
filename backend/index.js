29207ea4de8008ec465d8e6f5488d407d0a3927a
Jovan Simonovic <jovan.simonovic02@gmail.com>
2024-08-23 14:13:53 +0200

    Feature: Created Sign Up API, Auth Route and Auth Controller

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
 
 // connects to MongoDB Atlas
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(cors({ origin: "*" }));
 
 // routers
+app.use("/auth", authRouter);
 app.use("/user", userRouter);
 
 app.listen(process.env.PORT, () => {
