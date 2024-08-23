de1698fcadcbbf4d39f7bf0cb33b2b18d651a52b
Jovan Simonovic <jovan.simonovic02@gmail.com>
2024-08-23 12:39:19 +0200

    Feature: Created User Route and User Controller

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,25 @@
 import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
+import userRouter from "./routes/user.route.js";
 
+// connects to MongoDB Atlas
 mongoose
   .connect(process.env.MONGODB_URL)
   .then(() => console.log("Connected to MongoDB Atlas"))
   .catch((error) => console.log(error));
 
 const app = express();
+
+// middleware to parse JSON request bodies
+app.use(express.json());
+
+// cors configuration
+app.use(cors({ origin: "*" }));
+
+// routers
+app.use("/user", userRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
