15dc0cc90822ee19de5d732fe9488bd238a5e58a
Jovan Simonovic <jovan.simonovic02@gmail.com>
2024-08-31 14:06:14 +0200

    Feature: Implemented redux-persist

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./store/store.js";
+import { persistor, store } from "./store/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,11 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "../features/user/userSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import rootReducer from "./rootReducer";
 
+// redux-persist configuration
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+// creates a persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// creates a redux store
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware({
       serializableCheck: false,
     });
   },
 });
+
+export const persistor = persistStore(store);
