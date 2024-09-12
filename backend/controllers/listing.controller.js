eb5f730e8c5e0f680badb06fa126d13a97e456ea
Jovan Simonovic <jovan.simonovic02@gmail.com>
2024-09-12 23:27:34 +0200

    feature: Developed create listing API

--- backend/controllers/listing.controller.js
+++ backend/controllers/listing.controller.js
@@ -1,8 +1,22 @@
+import { errorHandler } from "../utils/error.js";
+import Listing from "../models/listing.model.js";
+
 export const getAllListings = async (req, res, next) => {};
 
 export const getListingById = async (req, res, next) => {};
 
-export const createListing = async (req, res, next) => {};
+export const createListing = async (req, res, next) => {
+  try {
+    const newListing = await Listing.create(req.body);
+    return res.status(201).json({
+      success: true,
+      message: "Listing created successfully",
+      listing: newListing,
+    });
+  } catch (error) {
+    next(errorHandler(500, error.message));
+  }
+};
 
 export const updateListing = async (req, res, next) => {};
 
--- backend/models/listing.model.js
+++ backend/models/listing.model.js
@@ -1,5 +1,22 @@
 import { Schema, model } from "mongoose";
 
-const listingSchema = new Schema({}, { timestamps: true });
+const listingSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    address: { type: String, required: true },
+    regularPrice: { type: Number, required: true },
+    discountPrice: { type: Number, required: false },
+    bathrooms: { type: Number, required: true },
+    bedrooms: { type: Number, required: true },
+    isFurnished: { type: Boolean, required: true },
+    hasParking: { type: Boolean, required: true },
+    type: { type: String, enum: ["rent", "sale"], required: true },
+    isOffered: { type: Boolean, required: true },
+    images: { type: [String], required: true },
+    createdBy: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 export default model("Listing", listingSchema);
