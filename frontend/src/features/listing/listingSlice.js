4b0c48888f2440757c285bd9cd3f67982369b348
Jovan Simonovic <jovan.simonovic02@gmail.com>
2024-09-19 13:39:37 +0200

    feature: created listing slice

---
+++ frontend/src/features/listing/listingSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  listing: null,
+  error: null,
+  loading: false,
+};
+
+export const listingSlice = createSlice({
+  name: "listing",
+  initialState,
+  reducers: {
+    createStart: (state) => {
+      state.error = null;
+      state.loading = true;
+    },
+    createSuccess: (state, action) => {
+      state.listing = action.payload;
+      state.error = null;
+      state.loading = false;
+    },
+    createFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+  },
+});
+
+export const { createStart, createSuccess, createFailure } =
+  listingSlice.actions;
+export default listingSlice.reducer;
