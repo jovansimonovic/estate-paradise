770bc071ab29f22ca087b20834f3f6130571e2af
Jovan Simonovic <jovan.simonovic02@gmail.com>
2024-09-19 13:39:24 +0200

    feature: added listingReducer to rootReducer

--- frontend/src/store/rootReducer.js
+++ frontend/src/store/rootReducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import userReducer from "../features/user/userSlice.js";
+import listingReducer from "../features/listing/listingSlice.js";
 
 // combines all reducers into one
 const rootReducer = combineReducers({
   user: userReducer,
+  listing: listingReducer,
 });
 
 export default rootReducer;
